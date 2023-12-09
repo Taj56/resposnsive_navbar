ece97acbef0b4163fdb06c1d22c639fa80447aa5
Taj56 <64923628+Taj56@users.noreply.github.com>
2023-12-08 20:30:12 -0500

    completed navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,32 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.querySelector('#nav-menu');
+const navToggle = document.querySelector('#nav-toggle');
+const navClose = document.querySelector('#nav-close');
 
 /* Menu show */
-
+navToggle.addEventListener('click', () =>{
+    navMenu.classList.add('show-menu')
+})
 
 /* Menu hidden */
-
+navClose.addEventListener('click', () =>{
+    navMenu.classList.remove('show-menu')
+})
 
 /*=============== SEARCH ===============*/
-
+const search = document.querySelector('#search')
+const searchBtn = document.querySelector('#search-btn')
+const searchClose = document.querySelector('#search-close')
 
 /* Search show */
-
+searchBtn.addEventListener('click', () =>{
+    search.classList.add('show-search')
+})
 
 /* Search hidden */
-
+searchClose.addEventListener('click', () =>{
+    search.classList.remove('show-search')
+})
 
 /*=============== LOGIN ===============*/
 
